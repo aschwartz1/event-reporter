1219cc0eb2a35467aa9a9903fec09b98050b6db7
Alex Schwartz <aschwartz@detaso.com>
2020-12-24 12:49:54 -0600

    Convert csv loader to a module

--- lib/csv_loader.rb
+++ lib/csv_loader.rb
@@ -1,20 +1,14 @@
 require 'csv'
 
-class CSVLoader
+module CSVLoader
   def load(filename="full_event_attendees.csv")
-    read_to_array(filename) if file_exists?(filename)
+    open(filename) if file_exists?(filename)
   end
 
   private
 
-  def read_to_array(filename)
-    contents = []
-
-    CSV.foreach(filename, csv_options) do |row|
-      contents << row
-    end
-
-    contents
+  def open(filename)
+    CSV.open filename, csv_options
   end
 
   def file_exists?(filename)
--- test/csv_loader_test.rb
+++ test/csv_loader_test.rb
@@ -2,13 +2,17 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/csv_loader'
 
+class MockLoader
+  include CSVLoader
+end
+
 class CSVLoaderTest < Minitest::Test
   def setup
-    @loader = CSVLoader.new
+    @loader = MockLoader.new
   end
 
   def test_it_exists
-    assert_instance_of CSVLoader, @loader
+    assert_instance_of MockLoader, @loader
   end
 
   def test_returns_nil_when_file_not_found
@@ -20,14 +24,12 @@ class CSVLoaderTest < Minitest::Test
   def test_it_loads_a_file_by_default
     contents = @loader.load
 
-    assert_instance_of Array, contents
-    assert_equal true, contents.all? { |row| row.is_a? CSV::Row }
+    assert_instance_of CSV, contents
   end
 
   def test_it_can_load_by_filename
     contents = @loader.load("full_event_attendees.csv")
 
-    assert_instance_of Array, contents
-    assert_equal true, contents.all? { |row| row.is_a? CSV::Row }
+    assert_instance_of CSV, contents
   end
 end
