a79a44ea7a73b200778aad96c931b0ba68ed6234
Alex Schwartz <aschwartz@detaso.com>
2020-12-31 10:30:07 -0600

    Revert find because need to rebuild it with new Attendee class instead of csv

--- lib/find.rb
+++ lib/find.rb
@@ -1,7 +1,4 @@
 class Find
-  def find(csv_array, header_symbol, search_value)
-    csv_array.select do |row|
-      row[header_symbol] == search_value
-    end
+  def initialize
   end
 end
--- test/find_test.rb
+++ test/find_test.rb
@@ -1,12 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/find.rb'
-require './lib/csv_loader.rb'
 
 class FindTest < Minitest::Test
   def setup
     @find = Find.new
-    @loader = CSVLoader.new
   end
 
   def test_it_exists
@@ -14,11 +12,5 @@ class FindTest < Minitest::Test
   end
 
   def test_it_finds_stuff
-    csv_array = @loader.load("full_event_attendees.csv")
-
-    found_records = @find.find(csv_array, :zipcode, "20011")
-
-    assert_instance_of Array, found_records
-    assert_equal true, found_records.all? { |row| row.is_a? CSV::Row }
   end
 end
