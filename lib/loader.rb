c6de3d81cf936486181243c3b64e89ea5d474f17
Alex Schwartz <aschwartz@detaso.com>
2021-01-01 22:06:55 -0600

    wip

--- lib/loader.rb
+++ lib/loader.rb
@@ -4,6 +4,45 @@ require './lib/attendee_list_builder'
 class Loader
   include CSVImporter
 
+  DIRECTIVES = [:load]
+
+  # def run(command_string)
+  #   # Validate string
+  #   parsed_command = parse_command(command_string)
+
+  #   # Pass command hash to run method
+  #   execute(parsed_command)
+  # end
+
+  def execute(parsed_command)
+    case parsed_command[:directive]
+    when :load
+      load(parsed_command[:arguments])
+    else
+      "Invalid directive"
+    end
+  end
+
+  def parse_command(command_string)
+    split_command = split_and_standardize(command_string)
+
+    if valid_command?(split_command)
+      command = split_command[0].to_sym
+      directive = split_command[1].to_sym
+      argument = split_command[2]
+
+      # Create hash
+      {
+        command: command,
+        directive: directive,
+        arguments: [argument]
+      }
+    else
+      nil
+    end
+  end
+
+
   def load(filename)
     # Load CSV
     csv = import(filename)
@@ -12,5 +51,45 @@ class Loader
     # TODO remove this dependency -- inject? Make ALB a module so I can `include` it?
     attendee_list_builder = AttendeeListBuilder.new(csv)
     attendee_list_builder.create_attendee_list
+  end
+
+  def split_and_standardize(command_string)
+    split_input = command_string.split
+
+    split_input << "full_event_attendees.csv" if split_input.length == 1
+    split_input.insert(1, "load")
+    split_input
+  end
+
+  private
+
+  def valid_command?(split_command)
+    valid_length = validate_command_length(split_command)
+    valid_directive = validate_command_directive(split_command)
+    valid_arguments = validate_command_arguments(split_command)
+
+    valid_length && valid_directive && valid_arguments
+  end
+
+  def validate_command_length(split_command)
+    [2..3].include?(split_command.length)
+  end
+
+  def validate_command_directive(split_command)
+    DIRECTIVES.include?(split_command[1])
+  end
+
+  def validate_command_arguments(split_command)
+    arguments = get_command_arguments(split_command)
+
+    return false unless arguments.length == 1
+    return false unless arguments[0].match?(/\A.+\.(csv)\z/)
+    true
+  end
+
+  def get_command_arguments(split_command)
+    split_command.delete_if do |word|
+      word == "load" || DIRECTIVES.include?(word.to_sym)
+    end
   end
 end
--- test/loader_test.rb
+++ test/loader_test.rb
@@ -11,10 +11,43 @@ class LoaderTest < Minitest::Test
     assert_instance_of Loader, @loader
   end
 
+  def test_can_parse_command_string
+    good_input = "load"
+    bad_input = "load not_a_filename"
+
+    good_expected = {
+      command: :load,
+      directive: :load,
+      arguments: ["full_event_attendees.csv"]
+    }
+
+    assert_equal good_expected, @loader.parse_command(good_input)
+    assert_nil @loader.parse_command(bad_input)
+  end
+
   def test_can_generate_attendees_from_csv
     attendee_list = @loader.load("test_good_data.csv")
 
     assert_equal 1, attendee_list.length
     assert_equal true, attendee_list.all? { |item| item.is_a? Attendee }
+  end
+
+  def test_can_standardize_command_input
+    implicit_filename_input = "load"
+    explicit_filename_input = "load foo.csv"
+
+    implicit_expected = ["load", "load", "full_event_attendees.csv"]
+    explicit_expected = ["load", "load", "foo.csv"]
+
+    assert_equal implicit_expected, @loader.split_and_standardize(implicit_filename_input)
+    assert_equal explicit_expected, @loader.split_and_standardize(explicit_filename_input)
+  end
+
+  def test_can_execute
+    skip
+    # TODO
+    # create a valid parsed command (hash)
+    # pass to method, make sure it works
+    # make sure it also returns error string if directive is invalid
   end
 end
